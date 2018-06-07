80587d9a845c33a5e424455f91e115ab9b18d41b
Gabriel Alarcon <galarconesp@gmail.com>
2018-06-07 19:34:24 -0400

    commit 2

---
+++ 1 metodos/gae.rb
@@ -0,0 +1,21 @@
+def random
+  result = [true, false].sample
+  puts result
+end
+
+puts random
+
+
+$f = "fran"
+
+def saludo
+
+	puts "Hola #{$f}"
+end
+
+saludo
+
+g = [1, 2, 3]
+
+a = g[-3]
+puts a
