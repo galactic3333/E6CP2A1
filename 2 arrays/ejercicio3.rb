c948087a2d422335521bcfbc3b89b14486eda40e
Gabriel Alarcon <galarconesp@gmail.com>
2018-06-08 00:55:02 -0400

    commit6

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -4,4 +4,47 @@
  # 3. Crear un método para obtener el promedio de un arreglo.
  # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 
-a = [1,2,3,9,1,4,5,2,3,6,6]
+$a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+puts 'Ingresa la opcion'
+x = gets.chomp.to_i
+
+if x == 1
+  def metod1
+    $a.each do |i|
+      $a.delete(i) if i.even?
+    end
+    print $a
+  end
+  metod1
+end
+
+if x == 2
+  def metod2
+    suma = 0
+    $a.each do |i|
+      suma += i
+    end
+    puts "Suma = #{suma}"
+  end
+  metod2
+end
+
+if x == 3
+  def metod3
+    suma = 0
+    $a.each do |i|
+      suma += i
+    end
+    puts "Promedio = #{suma.to_f / $a.count}"
+  end
+  metod3
+end
+
+if x == 4
+  b = []
+  $a.each do |i|
+    b.push(i + 1)
+  end
+  print b
+end
